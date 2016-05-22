8c9b04c99d2b7e1e85303d025229b7e81efe17a2
Kevin Lane <lane.kevin.a@gmail.com>
2016-05-21 23:24:40 -0700

    Add GUI support to Pythello script.

--- pythello.py
+++ pythello.py
@@ -1,19 +1,37 @@
 from game import GridGame, Othello
-from player import GreedyAI, RandomAI
+from player import Player, GreedyAI, RandomAI
+from gui import GUI
 
+players = 0
 size = 8
-games = 100
-verbose = False
+gui = True
+verbose = True
 
-player1 = GreedyAI('black')
-player2 = RandomAI('white')
+if players == 2:
+    player1 = Player('black')
+    player2 = Player('white')
+elif players == 1:
+    player1 = Player('black')
+    player2 = GreedyAI('white')
+else:
+    player1 = GreedyAI('black')
+    player2 = RandomAI('white')
 
-results = {player1: 0, player2: 0, GridGame.DRAW: 0}
 game = Othello(size, player1, player2, verbose)
 
-for i in range(games):
-    game.play()
-    results[game.winner] += 1
-    game.reset()
+if gui:
+    size = 500
+    margin = 50
+    GUI(game, size, margin).mainloop()
+elif players == 0:
+    games = 100
+    results = {player1: 0, player2: 0, GridGame.DRAW: 0}
 
-print(results)
+    for i in range(games):
+        game.play()
+        results[game.winner] += 1
+        game.reset()
+
+    print(results)
+else:
+    raise ValueError("Interactive mode only supported with the GUI")
