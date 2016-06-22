2049e52b1db2ae459e40f7f2ca31bc8d4a0d5d15
Kevin Lane <lane.kevin.a@gmail.com>
2016-06-21 22:43:36 -0700

    Add negamax algorithm as an AI strategy

--- ai.py
+++ ai.py
@@ -1,5 +1,39 @@
+import numpy as np
 import random
 from collections import defaultdict
+from copy import deepcopy
+
+
+class Negamax(object):
+    def __init__(self, depth, scoring):
+        self.depth = depth
+        self.scoring = scoring
+
+    def __call__(self, game):
+        return self.negamax(game, self.depth)[1]
+
+    def negamax(self, game, depth, alpha=-np.inf, beta=np.inf):
+        if depth == 0 or len(game.valid) == 0:
+            return [self.scoring(game)]
+
+        best_val = -np.inf
+        best_move = 0
+
+        for move in game.valid:
+            child = deepcopy(game)
+            child.move(move)
+            val = -self.negamax(child, depth - 1, -beta, -alpha)[0]
+
+            if val > best_val:
+                best_val = val
+                best_move = move
+
+            alpha = max(alpha, val)
+
+            if alpha >= beta:
+                break
+
+        return best_val, best_move
 
 
 def greedy_move(game):
