1b44f6b611cfc7bb6d4fbbd6085156f44d3caf91
Kevin Lane <lane.kevin.a@gmail.com>
2022-09-13 21:23:57 -0700

    Wrap enum values in an object

--- pythello/player/player.py
+++ pythello/player/player.py
@@ -1,7 +1,6 @@
 from __future__ import annotations
 
 from enum import Enum
-from functools import partial
 from typing import TYPE_CHECKING
 
 from pythello.player.greedy import greedy_move
@@ -10,13 +9,18 @@ from pythello.player.random import random_move
 
 if TYPE_CHECKING:
     from pythello.game import Game
-    from pythello.utils.typing import Position
+    from pythello.utils.typing import AIPlayer, Position
 
 
-class AI(Enum):
-    RANDOM = partial(random_move)
-    GREEDY = partial(greedy_move)
-    NEGAMAX = Negamax()
+class PlayerWrapper:
+    def __init__(self, player: AIPlayer) -> None:
+        self._player = player
 
     def __call__(self, game: Game) -> Position:
-        return self.value(game)
+        return self._player(game)
+
+
+class AI(PlayerWrapper, Enum):
+    RANDOM = PlayerWrapper(random_move)
+    GREEDY = PlayerWrapper(greedy_move)
+    NEGAMAX = PlayerWrapper(Negamax())
