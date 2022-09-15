4b09ea9f64262994d7b5c2a236e30fda510574d8
Kevin Lane <lane.kevin.a@gmail.com>
2022-09-15 15:14:29 -0700

    Directly reference board methods

--- pythello/score/score.py
+++ pythello/score/score.py
@@ -3,11 +3,12 @@ from __future__ import annotations
 from enum import Enum
 from typing import TYPE_CHECKING
 
+from pythello.board import Board
 from pythello.score.greedy import greedy_score
 from pythello.score.weighted import WeightedScore
 
 if TYPE_CHECKING:
-    from pythello.board import Board, Color
+    from pythello.board import Color
     from pythello.utils.typing import Scorer
 
 
@@ -23,10 +24,10 @@ class Score(ScorerWrapper, Enum):
     GREEDY = ScorerWrapper(greedy_score)
     EDGE = WeightedScore(
         {
-            lambda board, player: board.player_corners(player): 16,
-            lambda board, player: board.player_edges(player): 4,
+            Board.player_corners: 16,
+            Board.player_edges: 4,
             lambda board, player: len(board.valid_moves(player)): 2,
-            lambda board, player: board.player_interior(player): 1,
-            lambda board, player: board.player_frontier(player): -1,
+            Board.player_interior: 1,
+            Board.player_frontier: -1,
         }
     )
