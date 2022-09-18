d1a82475fcbc1fe9368329a469ca86cec0197fb8
Kevin Lane <lane.kevin.a@gmail.com>
2022-09-18 08:59:50 -0700

    Remove edge score class

--- pythello/score/__init__.py
+++ pythello/score/__init__.py
@@ -1,5 +1,4 @@
-from .edge import EdgeScore
 from .score import Score, Scorer
 from .weighted import WeightedScore
 
-__all__ = ['EdgeScore', 'Score', 'Scorer', 'WeightedScore']
+__all__ = ['Score', 'Scorer', 'WeightedScore']
