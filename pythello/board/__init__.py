0a376f991cf3c02b7118aeedd8305be4c05e4725
Kevin Lane <lane.kevin.a@gmail.com>
2022-09-26 09:53:29 -0700

    Add function to get the index of a position

--- pythello/board/__init__.py
+++ pythello/board/__init__.py
@@ -1,12 +1,15 @@
 from .board import Board
 from .color import Color
-from .position import Position, PositionSet, position_to_coordinates, split_position
+from .position import (
+    Position, PositionSet, position_index, position_to_coordinates, split_position
+)
 
 __all__ = [
     'Board',
     'Color',
     'Position',
     'PositionSet',
+    'position_index',
     'position_to_coordinates',
     'split_position',
 ]
